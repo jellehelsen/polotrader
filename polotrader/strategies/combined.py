c1587b2db0071759b69c1eecf396c5d1b7f37536
Jelle Helsen <jelle@playpass.be>
2017-09-12 10:38:20 +0200

    strategy finetuning

--- polotrader/strategies/combined.py
+++ polotrader/strategies/combined.py
@@ -7,8 +7,8 @@ class Combined(Strategy):
                 dataframe['close'].values, dataframe['volume'].values)
 
         dataframe['upper'], dataframe['middle'], dataframe['lower'] = talib.BBANDS(dataframe['close'].values, timeperiod=50, matype=talib.MA_Type.SMA)
-        dataframe['sma10'] = dataframe['close'].rolling(10).mean()
-        dataframe['sma100'] = dataframe['close'].rolling(100).mean()
+        dataframe['sma10'] = dataframe['close'].rolling(5).mean()
+        dataframe['sma100'] = dataframe['close'].rolling(50).mean()
         dataframe['signal'] = 0
         for i in range(len(dataframe)):
             row = dataframe.iloc[i]
@@ -17,11 +17,11 @@ class Combined(Strategy):
                 dataframe.iloc[i,-1] = 1
             elif row['close'] <= row['upper'] and prev_row['close'] > prev_row['upper']:
                 dataframe.iloc[i,-1] = -1
-            elif prev_row['mfi'] < 20 and row['mfi'] > prev_row['mfi']:
-                dataframe.iloc[i,-1] = 1
-            elif prev_row['mfi'] > 80 and row['mfi'] < prev_row['mfi']:
-                dataframe.iloc[i,-1] = -1
-            elif row['sma10'] < row['sma100'] and prev_row['sma10'] > prev_row['sma100']:
-                dataframe.iloc[i,-1] = -1
-            elif row['sma10'] > row['sma100'] and prev_row['sma10'] < prev_row['sma100']:
-                dataframe.iloc[i,-1] = 1
+            # elif prev_row['mfi'] < 20 and row['mfi'] > prev_row['mfi']:
+                # dataframe.iloc[i,-1] = 1
+            # elif prev_row['mfi'] > 80 and row['mfi'] < prev_row['mfi']:
+                # dataframe.iloc[i,-1] = -1
+            # elif row['sma10'] < row['sma100'] and prev_row['sma10'] > prev_row['sma100']:
+                # dataframe.iloc[i,-1] = -1
+            # elif row['sma10'] > row['sma100'] and prev_row['sma10'] < prev_row['sma100']:
+                # dataframe.iloc[i,-1] = 1
--- polotrader/strategies/mfi.py
+++ polotrader/strategies/mfi.py
@@ -2,10 +2,17 @@ from .strategy import Strategy
 import numpy
 
 class MFI(Strategy):
+    def calc_smi(self, highs, lows, closes, volumes):
+        pass
+        typical = (highs + lows + closes) / 3.0
+        raw_money_flow = typical * volumes
+
     def generate_signals(self, dataframe):
         dataframe['typical'] = (dataframe['high'] + dataframe['low'] + dataframe['close']) / 3.0
         dataframe['rmf'] = dataframe['typical'] * dataframe['volume']
 
+        dataframe['mfu14'] = 0
+        dataframe['mgd14'] = 0
         dataframe['mfu'] = 0
         dataframe['mfd'] = 0
         dataframe['mfi'] = numpy.nan
@@ -21,14 +28,16 @@ class MFI(Strategy):
             if i > 14:
                 mfu = dataframe.iloc[i-13:i+1, -4].sum()
                 mfd = dataframe.iloc[i-13:i+1, -3].sum()
-                if mfd == 0.0:
-                    mfd = 1e-6
+                dataframe.iloc[i,-6] = mfu
+                dataframe.iloc[i,-5] = mfd
+                # if mfd == 0.0:
+                    # mfd = 1e-6
                 mfr = mfu/mfd
                 dataframe.iloc[i, -2] = 100 - 100 / (1 + mfr)
             row = dataframe.iloc[i]
             prev_row = dataframe.iloc[i-1]
-            if row['mfi'] < 20:
+            if row['mfi'] < 20 and row['mfi'] > prev_row['mfi']:
                 dataframe.iloc[i,-1] = 1
-            elif row['mfi'] > 80:
+            elif row['mfi'] > 80 and row['mfi'] < prev_row['mfi']:
                 dataframe.iloc[i,-1] = -1
 
